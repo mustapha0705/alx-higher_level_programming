dd90b7bb9ca817b626a50bb19f486cc80ca8732b
Mustapha Mohammed <mustapha0705>
2024-03-16 08:24:07 -0700

    :)

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(i + 1, 10):
-        print('{:d}{:d}, '.format(i, j), end='')
-print('{:02d}\n'.format(98))
+for i in range(8):
+	for j in range(i + 1, 10):
+		if i != j:
+			print('{:d}{:d}, '.format(i, j), end='')
+print('{:02d}\n'.format(89))
