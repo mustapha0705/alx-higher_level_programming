dedd6043183bd50abc8e8e78626d93bfb1e1a476
Mustapha Mohammed <mustapha0705>
2024-03-16 07:53:28 -0700

    sum not 3

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(10):
-    for j in range(i+1, 10):
-        print(f"{i}{j}", end=", " if i < 9 or j < 8 else "\n")
-
+    for j in range(i + 1, 10):
+        print('{:02d}, '.format(i * 10 + j), end='')
+print('{:02d}\n'.format(99))
