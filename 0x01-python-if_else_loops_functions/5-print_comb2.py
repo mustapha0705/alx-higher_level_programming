8f3982f6576122cf7ed72e280a715fc22e393903
Mustapha Mohammed <mustapha0705>
2024-03-16 07:32:24 -0700

    just numbers

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 for i in range(100):
-	if i < 99:
-		if i < 10:
-			print(f'0{i}', end=', ')
-		else:
-			print(i, end=', ')
-	else:
-		print(i, end=' ')
+    print('{:02d}'.format(i), end=', ' if i < 99 else '\n')
+
