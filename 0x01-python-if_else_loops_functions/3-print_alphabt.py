03a8a83040464949717e66b80d5f31903979f615
Mustapha Mohammed <mustapha0705>
2024-03-16 07:20:49 -0700

    more ASCII

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for i in range (97, 123):
-	if chr(i) != 'e' and chr(i) != 'q':
-		print(chr(i), end=' ')
+print(''.join(['{}'.format(chr(i)) for i in range(97, 123) if chr(i) not in ['q', 'e']]), end='')
+
