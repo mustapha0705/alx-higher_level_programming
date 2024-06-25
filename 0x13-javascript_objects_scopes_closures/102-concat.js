dce15dccc10baadcc789a201ddce3651f4576221
Mustapha Mohammed <mustapha0705>
2024-06-25 06:07:03 -0700

    concatinate 2 files

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,41 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+if (process.argv.length !== 5) {
+  console.error('Usage: node 102-concat.js <fileA> <fileB> <destination>');
+  process.exit(1);
+}
+
+const fileAPath = process.argv[2];
+const fileBPath = process.argv[3];
+const destinationPath = process.argv[4];
+
+// Read fileA
+fs.readFile(fileAPath, 'utf8', (err, dataA) => {
+  if (err) {
+    console.error(`Error reading file ${fileAPath}:`, err.message);
+    process.exit(1);
+  }
+
+  // Read fileB
+  fs.readFile(fileBPath, 'utf8', (err, dataB) => {
+    if (err) {
+      console.error(`Error reading file ${fileBPath}:`, err.message);
+      process.exit(1);
+    }
+
+    // Concatenate contents
+    const concatenatedData = dataA + dataB;
+
+    // Write to destination file
+    fs.writeFile(destinationPath, concatenatedData, 'utf8', (err) => {
+      if (err) {
+        console.error(`Error writing to file ${destinationPath}:`, err.message);
+        process.exit(1);
+      }
+
+      console.log(`Files ${fileAPath} and ${fileBPath} concatenated successfully into ${destinationPath}`);
+    });
+  });
+});
